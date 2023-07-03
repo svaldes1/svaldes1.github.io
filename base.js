7af6aaeaca04c6237d0242f3ad0ab0131dd4581e
Sebastian Valdes <sebz2013@gmail.com>
2023-07-03 18:58:11 -0400

    implemented a SINGLE (1) (UNO) close button

--- base.js
+++ base.js
@@ -1,35 +1,35 @@
 
 
 window.addEventListener('load', () => {
-    if (localStorage.getItem('light-mode') === 'true') {
-      document.body.classList.add('light-mode-body');     
-    } else {
-        document.body.classList.remove('light-mode-body');
-    }
-  })
-  
+  if (localStorage.getItem('light-mode') === 'true') {
+    document.body.classList.add('light-mode-body');
+  } else {
+    document.body.classList.remove('light-mode-body');
+  }
+})
+
 
 function switchMode() {
- 
-    document.body.classList.toggle('light-mode-body');
 
-    if (document.body.classList.contains('light-mode-body')){
-        localStorage.setItem('light-mode', 'true');
+  document.body.classList.toggle('light-mode-body');
 
-    } else {
-        localStorage.setItem('light-mode', 'false');
-    }
- };
+  if (document.body.classList.contains('light-mode-body')) {
+    localStorage.setItem('light-mode', 'true');
 
- if (localStorage.getItem('light-mode') === 'true') {
-    document.body.classList.add('light-mode-body');
- };
- function openNav() {
-    document.getElementById("myNav").style.height = "100%";
-  };
-  
-  function closeNav() {
-    document.getElementById("myNav").style.height = "0%";
-  };
-  
-    
+  } else {
+    localStorage.setItem('light-mode', 'false');
+  }
+};
+
+if (localStorage.getItem('light-mode') === 'true') {
+  document.body.classList.add('light-mode-body');
+};
+function openNav() {
+  document.getElementById("myNav").style.height = "100%";
+};
+
+function closeNav() {
+  document.getElementById("myNav").style.height = "0%";
+};
+
+
