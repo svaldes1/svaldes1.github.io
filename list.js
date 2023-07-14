298f6c78e2dda27f48511870512ac2cd2c9650b4
Sebastian Valdes <sebz2013@gmail.com>
2023-07-14 13:55:37 -0400

    Updated List Functionality

--- list.js
+++ list.js
@@ -1,5 +1,7 @@
 let form = document.getElementById('addForm');
 let itemList = document.getElementById('items');
+let reps = document.getElementById('reps');
+let weight = document.getElementById('weight');
 
 
 // Call function 
@@ -8,18 +10,19 @@ form.addEventListener('submit', runEvent);
 let deleteBtn = document.getElementById('items');
     deleteBtn.addEventListener('click', removeItem);
 
-// Filter Items
-
 
 // Add item function
 function runEvent(e){
     e.preventDefault();
 
-    // Get input value
+    // Get input values
     let inputValue = document.getElementById('item').value;
+    let repsValue = document.getElementById('reps').value;
+    let weightValue = document.getElementById('weight').value;
+
 
     // validate input
-    if(inputValue === ''){
+    if(inputValue === '' || repsValue === '' || weightValue === ''){
         alert('Please enter a value');
         return;
     }
@@ -32,9 +35,20 @@ function runEvent(e){
 
     // add text to li
     li.appendChild(document.createTextNode(inputValue));
+    li.appendChild(document.createTextNode(' | Reps: '));
+    li.appendChild(document.createTextNode(repsValue));
+    li.appendChild(document.createTextNode('| Weight: '));
+    li.appendChild(document.createTextNode(weightValue));
     
     //push item
     itemList.appendChild(li);
+    var editButton = document.createElement('button');
+    editButton.appendChild(document.createTextNode('Edit'));
+    editButton.className = "edit";
+    editButton.addEventListener('click', function() {
+      editTriple(item, reps, weight, li);
+    });
+    li.appendChild(editButton);
     
     //create button
     let button = document.createElement('button');
@@ -45,21 +59,33 @@ function runEvent(e){
     //add classes to button;
     button.className = " trash delete";
 
+    button.addEventListener('click', function () {
+        // Remove the triple from the array
+        removeTriple(item, reps, weight, li);
+      });
+
     // push button to li
     li.appendChild(button);
 
+    // store in local storage
+    var item = document.getElementById('item').value;
+    var reps = document.getElementById('reps').value;
+    var weight = document.getElementById('weight').value;
+
+    var triples = JSON.parse(localStorage.getItem('triples')) || [];
+
+    triples.push({ item: item, reps: reps, weight: weight });
+
+    localStorage.setItem('triples', JSON.stringify(triples));
+
+
+
     // clear input
 
+
     document.getElementById('item').value = '';
+    document.getElementById('reps').value = '';
+    document.getElementById('weight').value = '';
 
 }
 
-// function remove item
-
-function removeItem(e){
-    if(e.target.classList.contains('delete')){
-            let li = e.target.parentElement;
-            itemList.removeChild(li);
-    }
-    console.log(e.target.parentElement);
-}
