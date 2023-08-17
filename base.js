088134bf6152ab080634562a969ed44a78ebcf3a
Sebastian Valdes <sebz2013@gmail.com>
2023-08-17 14:01:05 -0400

    Updates to New Div Creation

--- base.js
+++ base.js
@@ -1,71 +1,5 @@
 
-window.onload = function () {
-  // Retrieve stored triples from local storage
-  var pageKey = getPageSpecificKey();
-  var triples = JSON.parse(localStorage.getItem(pageKey)) || [];
 
-  // Display the stored triples on the page
-  triples.forEach(function (triple) {
-    displayTriple(triple.item, triple.reps, triple.weight);
-  });
-}
-function displayTriple(item, reps, weight) {
-  var ul = document.getElementById('items');
-  var li = document.createElement('li');
-  li.appendChild(document.createTextNode(item));
-  li.appendChild(document.createTextNode(' | Reps: '));
-  li.appendChild(document.createTextNode(reps));
-  li.appendChild(document.createTextNode('| Weight: '));
-  li.appendChild(document.createTextNode(weight));
-
-  //push item
-  ul.appendChild(li);
-
-  var editButton = document.createElement('button');
-  editButton.appendChild(document.createTextNode('Edit'));
-  editButton.addEventListener('click', function () {
-    editTriple(item, reps, weight, li);
-  });
-  editButton.className = "edit";
-  li.appendChild(editButton);
-  //create button
-  let button = document.createElement('button');
-
-  //add Text to botton
-  button.appendChild(document.createTextNode('×'));
-
-  button.addEventListener('click', function () {
-    // Remove the triple from the array
-    removeTriple(item, reps, weight, li);
-  });
-
-  //add classes to button;
-  button.className = " trash delete";
-
-  // push button to li
-  li.appendChild(button);
-}
-function removeTriple(item, reps, weight, listItem) {
-  // Retrieve stored triples from local storage
-  var pageKey = getPageSpecificKey();
-  var triples = JSON.parse(localStorage.getItem(pageKey)) || [];
-
-  // Find the index of the triple to be removed
-  var index = triples.findIndex(function (triple) {
-    return triple.item === item && triple.reps === reps && triple.weight === weight;
-  });
-
-  // Remove the triple from the array if found
-  if (index > -1) {
-    triples.splice(index, 1);
-  }
-
-  // Save the updated triples to local storage
-  localStorage.setItem(pageKey, JSON.stringify(triples));
-
-  // Remove the list item from the list
-  listItem.remove();
-}
 
 
 window.addEventListener('load', () => {
@@ -101,111 +35,112 @@ function closeNav() {
   document.getElementById("myNav").style.height = "0%";
 };
 
-function editTriple(item, reps, weight, listItem) {
-  // Create an edit form
-  var editForm = document.createElement('form');
-
-  // Create input fields for the triple components
-  var itemInput = document.createElement('input');
-  itemInput.type = 'text';
-  itemInput.value = item;
-
-  var repsInput = document.createElement('input');
-  repsInput.type = 'text';
-  repsInput.value = reps;
-
-  var weightInput = document.createElement('input');
-  weightInput.type = 'text';
-  weightInput.value = weight;
-
-  // Create a save button
-  var saveButton = document.createElement('button');
-  saveButton.textContent = 'Save';
-  saveButton.type = 'button';
-
-  // Event listener for the form submission
-  saveButton.addEventListener('click', function () {
-    // Get the updated values from the input fields
-    var updatedItem = itemInput.value;
-    var updatedReps = repsInput.value;
-    var updatedWeight = weightInput.value;
-
-    // Update the triple in the list
-    listItem.textContent = updatedItem + " | Reps: " + updatedReps + "| Weight: " + updatedWeight;
-
-    // Update the triple in local storage
-    updateTriple(item, reps, weight, updatedItem, updatedReps, updatedWeight);
-
-    // Remove the edit form
-    editForm.remove();
 
 
-    var editButton = document.createElement('button');
-    editButton.textContent = 'Edit';
-    editButton.className = "edit";
-    editButton.addEventListener('click', function () {
-      editTriple(item, reps, weight, listItem);
-    });
-    listItem.appendChild(editButton);
+// function closeDiv(divId) {
 
-    // Create a close button
-    var closeButton = document.createElement('button');
-    closeButton.textContent = 'x';
-    closeButton.className = " trash delete";
-    closeButton.addEventListener('click', function () {
-      listItem.remove();
-      removeTriple(item, reps, weight, listItem);
-    });
-    listItem.appendChild(closeButton);
-  });
-  editForm.appendChild(itemInput);
-  editForm.appendChild(repsInput);
-  editForm.appendChild(weightInput);
-  editForm.appendChild(saveButton);
+//   event.stopPropagation();
+//   const div = document.getElementById(divId);
+//   if (div) {
+//     div.style.display = 'none';
+//     localStorage.setItem(divId, 'closed');
+//   }
+// }
 
-  // Replace the list item with the edit form
-  listItem.innerHTML = '';
-  listItem.appendChild(editForm);
+// // Check local storage for closed divs on page load
+// window.addEventListener('load', () => {
+//   const closedDivs = Object.keys(localStorage);
+//   closedDivs.forEach(divId => {
+//     const div = document.getElementById(divId);
+//     if (div && localStorage.getItem(divId) === 'closed') {
+//       div.style.display = 'none';
+//     } 
+//   });
+// });
 
+// let divCount = 8;
+
+// // Function to add a new div with unique divID
+// function addNewDiv() {
+//   divCount++;
+//   const divId = `div${divCount}`;
+
+//   const container = document.getElementById('container');
+  
+//   const newDiv = document.createElement('div');
+//   newDiv.classList.add('card');
+//   newDiv.classList.add('card-wide');
+//   newDiv.id = divId;
+
+//   newDiv.innerHTML = "New Routine";
+  
+//   const closeButton = document.createElement('span');
+//   closeButton.classList.add('close-button');
+//   closeButton.innerHTML = '&times;';
+//   closeButton.onclick = () => closeDiv(divId);
+
+
+//   newDiv.appendChild(closeButton);
+
+//   // Attach the onclick function to navigate to the page
+//   newDiv.onclick = () => navigateToPage(divId);
+
+//   container.append(newDiv);
+
+//   localStorage.setItem(divId, 'open');
+
+// }
+
+function createDiv() {
+  const divContainer = document.getElementById('container');
+  const newDiv = document.createElement('div');
+  const divId = 'div_' + Date.now(); // Generate a unique ID using timestamp
+  newDiv.id = divId;
+  newDiv.classList.add('card');
+  newDiv.classList.add('card-wide');
+  
+  
+  newDiv.onclick = () => navigateToPage(divId);
+  const divTitle = localStorage.getItem(divId);
+  divContainer.appendChild(newDiv);
+  document.getElementById(divId).textContent = divTitle || "New Routine";
+  newDiv.innerHTML += `<span class="close-button" onclick="removeDiv('${divId}')">X</span>`;
+  saveDivToLocalStorage(divId);
 }
-function updateTriple(oldItem, oldReps, oldWeight, newItem, newReps, newWeight) {
-  // Retrieve stored triples from local storage
-  var pageKey = getPageSpecificKey();
-  var triples = JSON.parse(localStorage.getItem(pageKey)) || [];
-
-  // Find the index of the triple to be updated
-  var index = triples.findIndex(function (triple) {
-    return triple.item === oldItem && triple.reps === oldReps && triple.weight === oldWeight;
-  });
-
-  // Update the triple in the array if found
-  if (index > -1) {
-    triples[index].item = newItem;
-    triples[index].reps = newReps;
-    triples[index].weight = newWeight;
-  }
-
-  // Save the updated triples to local storage
-  localStorage.setItem(pageKey, JSON.stringify(triples));
+function saveDivToLocalStorage(divId) {
+  const divs = JSON.parse(localStorage.getItem('divs')) || [];
+  divs.push(divId);
+  localStorage.setItem('divs', JSON.stringify(divs));
 }
-
-function closeDiv(divId) {
-
+function removeDiv(divId) {
   event.stopPropagation();
-  const div = document.getElementById(divId);
-  if (div) {
-    div.style.display = 'none';
-    localStorage.setItem(divId, 'closed');
+  const divContainer = document.getElementById('container');
+  const divToRemove = document.getElementById(divId);
+  divContainer.removeChild(divToRemove);
+
+  const divs = JSON.parse(localStorage.getItem('divs')) || [];
+  const index = divs.indexOf(divId);
+  if (index !== -1) {
+      divs.splice(index, 1);
+      localStorage.setItem('divs', JSON.stringify(divs));
   }
 }
-
-// Check local storage for closed divs on page load
-window.addEventListener('load', () => {
-  const closedDivs = Object.keys(localStorage);
-  closedDivs.forEach(divId => {
-    const div = document.getElementById(divId);
-    if (div && localStorage.getItem(divId) === 'closed') {
-      div.style.display = 'none';
-    }
+function loadDivsFromLocalStorage() {
+  const divs = JSON.parse(localStorage.getItem('divs')) || [];
+  const divContainer = document.getElementById('container');
+  divs.forEach(divId => {
+      const newDiv = document.createElement('div');
+      newDiv.id = divId;
+      const divTitle = localStorage.getItem(divId);
+      divContainer.appendChild(newDiv);
+      document.getElementById(divId).textContent = divTitle || "New Routine";
+      newDiv.innerHTML += `<span class="close-button" onclick="removeDiv('${divId}')">X</span>`;
+      
+      newDiv.classList.add('card');
+      newDiv.classList.add('card-wide');
+      
+      newDiv.onclick = () => navigateToPage(divId);
   });
-});
+}
+window.addEventListener('load', loadDivsFromLocalStorage);
+
--- list.js
+++ list.js
@@ -2,6 +2,162 @@ let form = document.getElementById('addForm');
 let itemList = document.getElementById('items');
 let reps = document.getElementById('reps');
 let weight = document.getElementById('weight');
+window.onload = function () {
+  // Retrieve stored triples from local storage
+  var pageKey = getPageSpecificKey();
+  var triples = JSON.parse(localStorage.getItem(pageKey)) || [];
+
+  // Display the stored triples on the page
+  triples.forEach(function (triple) {
+    displayTriple(triple.item, triple.reps, triple.weight);
+  });
+}
+function displayTriple(item, reps, weight) {
+  var ul = document.getElementById('items');
+  var li = document.createElement('li');
+  li.appendChild(document.createTextNode(item));
+  li.appendChild(document.createTextNode(' | Reps: '));
+  li.appendChild(document.createTextNode(reps));
+  li.appendChild(document.createTextNode('| Weight: '));
+  li.appendChild(document.createTextNode(weight));
+
+  //push item
+  ul.appendChild(li);
+
+  var editButton = document.createElement('button');
+  editButton.appendChild(document.createTextNode('Edit'));
+  editButton.addEventListener('click', function () {
+    editTriple(item, reps, weight, li);
+  });
+  editButton.className = "edit";
+  li.appendChild(editButton);
+  //create button
+  let button = document.createElement('button');
+
+  //add Text to botton
+  button.appendChild(document.createTextNode('×'));
+
+  button.addEventListener('click', function () {
+    // Remove the triple from the array
+    removeTriple(item, reps, weight, li);
+  });
+
+  //add classes to button;
+  button.className = " trash delete";
+
+  // push button to li
+  li.appendChild(button);
+}
+function removeTriple(item, reps, weight, listItem) {
+  // Retrieve stored triples from local storage
+  event.stopPropagation();
+  var pageKey = getPageSpecificKey();
+  var triples = JSON.parse(localStorage.getItem(pageKey)) || [];
+
+  // Find the index of the triple to be removed
+  var index = triples.findIndex(function (triple) {
+    return triple.item === item && triple.reps === reps && triple.weight === weight;
+  });
+
+  // Remove the triple from the array if found
+  if (index > -1) {
+    triples.splice(index, 1);
+  }
+
+  // Save the updated triples to local storage
+  localStorage.setItem(pageKey, JSON.stringify(triples));
+
+  // Remove the list item from the list
+  listItem.remove();
+}
+function editTriple(item, reps, weight, listItem) {
+  // Create an edit form
+  var editForm = document.createElement('form');
+
+  // Create input fields for the triple components
+  var itemInput = document.createElement('input');
+  itemInput.type = 'text';
+  itemInput.value = item;
+
+  var repsInput = document.createElement('input');
+  repsInput.type = 'text';
+  repsInput.value = reps;
+
+  var weightInput = document.createElement('input');
+  weightInput.type = 'text';
+  weightInput.value = weight;
+
+  // Create a save button
+  var saveButton = document.createElement('button');
+  saveButton.textContent = 'Save';
+  saveButton.type = 'button';
+
+  // Event listener for the form submission
+  saveButton.addEventListener('click', function () {
+    // Get the updated values from the input fields
+    var updatedItem = itemInput.value;
+    var updatedReps = repsInput.value;
+    var updatedWeight = weightInput.value;
+
+    // Update the triple in the list
+    listItem.textContent = updatedItem + " | Reps: " + updatedReps + "| Weight: " + updatedWeight;
+
+    // Update the triple in local storage
+    updateTriple(item, reps, weight, updatedItem, updatedReps, updatedWeight);
+
+    // Remove the edit form
+    editForm.remove();
+
+
+    var editButton = document.createElement('button');
+    editButton.textContent = 'Edit';
+    editButton.className = "edit";
+    editButton.addEventListener('click', function () {
+      editTriple(item, reps, weight, listItem);
+    });
+    listItem.appendChild(editButton);
+
+    // Create a close button
+    var closeButton = document.createElement('button');
+    closeButton.textContent = 'x';
+    closeButton.className = " trash delete";
+    closeButton.addEventListener('click', function () {
+      listItem.remove();
+      removeTriple(item, reps, weight, listItem);
+    });
+    listItem.appendChild(closeButton);
+  });
+  editForm.appendChild(itemInput);
+  editForm.appendChild(repsInput);
+  editForm.appendChild(weightInput);
+  editForm.appendChild(saveButton);
+
+  // Replace the list item with the edit form
+  listItem.innerHTML = '';
+  listItem.appendChild(editForm);
+
+}
+function updateTriple(oldItem, oldReps, oldWeight, newItem, newReps, newWeight) {
+  // Retrieve stored triples from local storage
+  var pageKey = getPageSpecificKey();
+  var triples = JSON.parse(localStorage.getItem(pageKey)) || [];
+
+  // Find the index of the triple to be updated
+  var index = triples.findIndex(function (triple) {
+    return triple.item === oldItem && triple.reps === oldReps && triple.weight === oldWeight;
+  });
+
+  // Update the triple in the array if found
+  if (index > -1) {
+    triples[index].item = newItem;
+    triples[index].reps = newReps;
+    triples[index].weight = newWeight;
+  }
+
+  // Save the updated triples to local storage
+  localStorage.setItem(pageKey, JSON.stringify(triples));
+}
+
 
 
 function getPageSpecificKey() {
@@ -11,8 +167,6 @@ function getPageSpecificKey() {
 // Call function 
 form.addEventListener('submit', runEvent);
 
-let deleteBtn = document.getElementById('items');
-    deleteBtn.addEventListener('click', removeItem);
 
 
 // Add item function
@@ -93,4 +247,3 @@ function runEvent(e){
     document.getElementById('weight').value = '';
 
 }
-
